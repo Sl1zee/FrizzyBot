a3ac42a54608e2ff603136de8c6ecdf8ebcfd49b
Sl1zee <dtoropccin@gmail.com>
2024-10-12 00:32:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import telebot
-
+from telebot import types
 bot = telebot.TeleBot('7939166697:AAFSCxD1ruXcbmYGcO4bEFCMnVl2MyK7R7U')
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -19,12 +19,16 @@ def start(message):
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
     if message.text == "Сделать заказ":
-        bot.send_message(message.chat.id, "Отправьте продавцу сообщеие с товаром из группы Frizzy Shop и укажите размер.")
+        bot.send_message(message.chat.id, "Отправьте продавцу сообщеие с товаром из группы Frizzy Shop и укажите размер.\nПример:")
+        file = open('./benzo.jpg', 'rb')
+        bot.send_photo(message.chat.id, file)
         markup = types.InlineKeyboardMarkup()
         markup.add(types.InlineKeyboardButton('Сделать заказ', url='https://t.me/LilFrizy'))
         bot.send_message(message.chat.id, "Заказать ТУТ:", reply_markup=markup)             
     if message.text == "Сделать заказ с Poizon":
-        bot.send_message(message.chat.id, "Отправьте продавцу сообщеие с фото товара, артикул и укажите размер.")
+        bot.send_message(message.chat.id, "Отправьте продавцу сообщеие с фото товара, артикул и укажите размер.\nПример:")
+        file1 = open('./nike.jpg', 'rb')
+        bot.send_photo(message.chat.id, file1)
         markup = types.InlineKeyboardMarkup()
         markup.add(types.InlineKeyboardButton('Сделать заказ с Poizon', url='https://t.me/LilFrizy'))
         bot.send_message(message.chat.id, "Заказать ТУТ:", reply_markup=markup)
