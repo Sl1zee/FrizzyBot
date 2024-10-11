07da5bb3d971f6870d3e6ad4a56d5cfd5f54a623
Sl1zee <dtoropccin@gmail.com>
2024-10-11 23:41:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import telebot
-from telebot import types
 
 bot = telebot.TeleBot('7939166697:AAFSCxD1ruXcbmYGcO4bEFCMnVl2MyK7R7U')
 
@@ -7,14 +6,14 @@ bot = telebot.TeleBot('7939166697:AAFSCxD1ruXcbmYGcO4bEFCMnVl2MyK7R7U')
 def start(message):
     bot.send_message(message.chat.id, "Добро пожаловать в магазин одежды Frizzy Shop!")
     
-    markup = types.ReplyKeyboardMarkup()
+    markup = telebot.types.ReplyKeyboardMarkup()
 
-    btn1 = types.KeyboardButton("Сделать заказ")
-    btn2 = types.KeyboardButton("Сделать заказ с Poizon")
+    btn1 = telebot.types.KeyboardButton("Сделать заказ")
+    btn2 = telebo.types.KeyboardButton("Сделать заказ с Poizon")
     markup.row(btn1)
     markup.row(btn2)
-    btn3 = types.KeyboardButton("Написать отзыв")
-    btn4 = types.KeyboardButton("Чат с поддержкой")
+    btn3 = telebot.types.KeyboardButton("Написать отзыв")
+    btn4 = telebot.types.KeyboardButton("Чат с поддержкой")
     markup.row(btn3, btn4)
     
     bot.send_message(message.chat.id, "Выберите итересующие вас действие:", reply_markup=markup)
@@ -23,24 +22,24 @@ def start(message):
 def handle_message(message):
     if message.text == "Сделать заказ":
         bot.send_message(message.chat.id, "Отправьте продавцу сообщеие с товаром из группы Frizzy Shop и укажите размер.")
-        markup = types.InlineKeyboardMarkup()
-        markup.add(types.InlineKeyboardButton('Сделать заказ', url='https://t.me/LilFrizy'))
+        markup = telebot.types.InlineKeyboardMarkup()
+        markup.add(telebot.types.InlineKeyboardButton('Сделать заказ', url='https://t.me/LilFrizy'))
         bot.send_message(message.chat.id, "Заказать ТУТ:", reply_markup=markup)             
 
     if message.text == "Сделать заказ с Poizon":
         bot.send_message(message.chat.id, "Отправьте продавцу сообщеие с фото товара, артикул и укажите размер.p")
-        markup = types.InlineKeyboardMarkup()
-        markup.add(types.InlineKeyboardButton('Сделать заказ с Poizon', url='https://t.me/LilFrizy'))
+        markup = telebot.types.InlineKeyboardMarkup()
+        markup.add(telebot.types.InlineKeyboardButton('Сделать заказ с Poizon', url='https://t.me/LilFrizy'))
         bot.send_message(message.chat.id, "Заказать ТУТ:", reply_markup=markup)
 
     if message.text == "Написать отзыв":
-        markup = types.InlineKeyboardMarkup()
-        markup.add(types.InlineKeyboardButton('Отзыв', url='https://t.me/FrizzySho_Otzov_Bot'))
+        markup = telebot.types.InlineKeyboardMarkup()
+        markup.add(telebot.types.InlineKeyboardButton('Отзыв', url='https://t.me/FrizzySho_Otzov_Bot'))
         bot.send_message(message.chat.id, "Написать отзыв:", reply_markup=markup) 
 
     if message.text == "Чат с поддержкой":
-        markup = types.InlineKeyboardMarkup()
-        markup.add(types.InlineKeyboardButton('Поддержка в ТГ', url='https://t.me/LilFrizy'))
+        markup = telebot.types.InlineKeyboardMarkup()
+        markup.add(telebot.types.InlineKeyboardButton('Поддержка в ТГ', url='https://t.me/LilFrizy'))
         bot.send_message(message.chat.id, "Возникли вопросы? Ответим ТУТ:", reply_markup=markup) 
 
 if __name__ == "__main__":
