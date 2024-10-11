fb3aa362759c4af2005ad669e51f12b5087d737e
Sl1zee <dtoropccin@gmail.com>
2024-10-11 23:57:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import telebot
 from telebot import types
-
 bot = telebot.TeleBot('7939166697:AAFSCxD1ruXcbmYGcO4bEFCMnVl2MyK7R7U')
-
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, "Добро пожаловать в магазин одежды Frizzy Shop!")
     
     markup = types.ReplyKeyboardMarkup()
-
     btn1 = types.KeyboardButton("Сделать заказ")
     btn2 = types.KeyboardButton("Сделать заказ с Poizon")
     markup.row(btn1)
@@ -26,22 +23,18 @@ def handle_message(message):
         markup = types.InlineKeyboardMarkup()
         markup.add(types.InlineKeyboardButton('Сделать заказ', url='https://t.me/LilFrizy'))
         bot.send_message(message.chat.id, "Заказать ТУТ:", reply_markup=markup)             
-
     if message.text == "Сделать заказ с Poizon":
-        bot.send_message(message.chat.id, "Отправьте продавцу сообщеие с фото товара, артикул и укажите размер.p")
+        bot.send_message(message.chat.id, "Отправьте продавцу сообщеие с фото товара, артикул и укажите размер.")
         markup = types.InlineKeyboardMarkup()
         markup.add(types.InlineKeyboardButton('Сделать заказ с Poizon', url='https://t.me/LilFrizy'))
         bot.send_message(message.chat.id, "Заказать ТУТ:", reply_markup=markup)
-
     if message.text == "Написать отзыв":
         markup = types.InlineKeyboardMarkup()
         markup.add(types.InlineKeyboardButton('Отзыв', url='https://t.me/FrizzySho_Otzov_Bot'))
         bot.send_message(message.chat.id, "Написать отзыв:", reply_markup=markup) 
-
     if message.text == "Чат с поддержкой":
         markup = types.InlineKeyboardMarkup()
         markup.add(types.InlineKeyboardButton('Поддержка в ТГ', url='https://t.me/LilFrizy'))
         bot.send_message(message.chat.id, "Возникли вопросы? Ответим ТУТ:", reply_markup=markup) 
-
 if __name__ == "__main__":
     bot.polling(none_stop=True)
